76e17e0017bfd9c9cb35e0782f6f335a671f2b50
Sam Pohlenz <sam@sampohlenz.com>
2009-07-10 15:44:32 +0930

    Separate module/class definition of Recliner::Database

--- lib/recliner/database.rb
+++ lib/recliner/database.rb
@@ -1,37 +1,39 @@
-class Recliner::Database
-  attr_reader :uri
+module Recliner
+  class Database
+    attr_reader :uri
   
-  def initialize(uri)
-    @uri = uri
-    create_database_if_missing!
-  end
+    def initialize(uri)
+      @uri = uri
+      create_database_if_missing!
+    end
   
-  def get(id, params={})
-    Recliner.get("#{uri}/#{id}", params)
-  end
+    def get(id, params={})
+      Recliner.get("#{uri}/#{id}", params)
+    end
   
-  def post(path, payload)
-    Recliner.post("#{uri}/#{path}", payload)
-  end
+    def post(path, payload)
+      Recliner.post("#{uri}/#{path}", payload)
+    end
   
-  def put(path, payload)
-    Recliner.put("#{uri}/#{path}", payload)
-  end
+    def put(path, payload)
+      Recliner.put("#{uri}/#{path}", payload)
+    end
   
-  def delete(path)
-    Recliner.delete("#{uri}/#{path}")
-  end
+    def delete(path)
+      Recliner.delete("#{uri}/#{path}")
+    end
   
-  def delete!
-    Recliner.delete(uri)
-  end
+    def delete!
+      Recliner.delete(uri)
+    end
   
-  def create!
-    Recliner.put(uri)
-  end
+    def create!
+      Recliner.put(uri)
+    end
 
-private
-  def create_database_if_missing!
-    create! rescue nil
+  private
+    def create_database_if_missing!
+      create! rescue nil
+    end
   end
 end
